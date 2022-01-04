9d20b764d142a489dade08cf5332ed3368fcb1d8
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-01-04 20:46:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// var number = prompt("Enter a number");
+// document.write("Number :",number + "<br>")
+// document.write ("Round :", Math.round(number) + "<br>");
+// document.write("Floor :",Math.floor(number)  + "<br>");
+// document.write("Ceil :",Math.ceil(number)  + "<br>");
+
+// var posNum = +prompt("Enter any positive number");
+
+// document.write("Number: " + Number(posNum) + "<br>");
+// document.write("Round: " + Math.round(posNum) + "<br>");
+// document.write("Floor: " + Math.floor(posNum) + "<br>");
+// document.write("Ceil: " + Math.ceil(posNum));
+
+
+// var number = prompt("Enter a negative number");
+// document.write("Number :",number + "<br>")
+// document.write ("Round :", Math.round(number) + "<br>");
+// document.write("Floor :",Math.floor(number) + "<br>");
+// document.write("Ceil :",Math.ceil(number)  + "<br>");
+
+// var a = prompt("Enter a value in +/-")
+// console.log(Math.abs(a))
+
+// var a = Math.random()*6;
+// document.write("Random dies value is ",Math.round(a))
+
+// var headUser = prompt("Enter a HeadUser Name");
+// var tailUser = prompt("Enter a tailUser Name");
+// var toss =Math.random() *2;
+// var floor = Math.floor(toss)
+// if(floor == 1){
+//     document.write("Head " +headUser +"Won the toss")
+// } else if(floor == 0){
+//     document.write("Tail" +tailUser +"Won the toss")
+// }
+
+// var a =Math.random(a)
+// console.log(a)
+
+var userWeight = +prompt("Enter your weight user")
+console.log("The weight of the user is " + userWeight + " kilograms")
